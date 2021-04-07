02495f660d3e8771e11632f177dad27f7cb76849
iree <uo269570@uniovi.es>
2021-04-07 17:38:53 +0200

    SDI-2021-402-9.2-Redirección de peticiones.

--- app.js
+++ app.js
@@ -95,6 +95,10 @@ require("./routes/rcanciones.js")(app,swig,gestorBD); // (app, param1, param2, e
 require("./routes/rautores.js")(app,swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app,swig, gestorBD);
 
+app.get('/', function (req, res) {
+    res.redirect('/tienda');
+})
+
 // lanzar el servidor
 app.listen(app.get('port'), function(){
     console.log('Servidor activo');
